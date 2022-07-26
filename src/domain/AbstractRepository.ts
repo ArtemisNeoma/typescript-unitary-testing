1ee306ccf3907a9300047b882ec8615742703a5b
Neo-WRN <nicolas.destito@warren.com.br>
2022-07-26 15:24:15 -0300

    refactor: fixed return type on create from AbstractRepository

--- src/domain/AbstractRepository.ts
+++ src/domain/AbstractRepository.ts
@@ -8,7 +8,7 @@ export default abstract class AbstractRepository
   implements IEndPointsRepository
 {
   protected _database: IVariableDatabase = new Map<number, IUser>();
-  abstract create(entity: object): void;
+  abstract create(entity: object): object;
   abstract read(id: number): object | undefined;
   abstract readAll(): IVariableDatabase;
   abstract update(id: number, newEntity: object): void;
