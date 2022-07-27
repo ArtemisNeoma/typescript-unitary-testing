5ede8817f6a1d3f612b2243f69bff3f0ba4d96e7
Neo-WRN <nicolas.destito@warren.com.br>
2022-07-27 11:04:26 -0300

    refactor: added +1 spaces long result logic for calcCpfDigit

--- src/util/validation/Cpf/calcCpfDigit.ts
+++ src/util/validation/Cpf/calcCpfDigit.ts
@@ -1,10 +1,17 @@
-const calcCpfDigit = (cpfArray: number[], start = 1): number =>
-  cpfArray.reduce(
-    (previousValue: number, currentValue: number, currentIndex: number) => {
-      const totalSum = previousValue + currentValue * (currentIndex + start);
-      return totalSum;
-    },
-    0,
-  ) % 11;
+const calcCpfDigit = (cpfArray: number[], start = 1): number => {
+  const result =
+    cpfArray.reduce(
+      (previousValue: number, currentValue: number, currentIndex: number) => {
+        const totalSum = previousValue + currentValue * (currentIndex + start);
+        return totalSum;
+      },
+      0,
+    ) % 11;
+  if (result >= 10) {
+    const resultString = result.toString();
+    return Number(resultString.slice(-1));
+  }
+  return result;
+};
 
 export default calcCpfDigit;
