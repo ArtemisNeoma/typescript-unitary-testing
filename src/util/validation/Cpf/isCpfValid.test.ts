5d65c8005e1d9d2a3c42903bbd94c76c260ddab8
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-02 17:27:27 -0300

    refactor: updated isCpfValid, and adapted it's test, to return boolean

--- src/util/validation/Cpf/isCpfValid.test.ts
+++ src/util/validation/Cpf/isCpfValid.test.ts
@@ -3,16 +3,16 @@ import isCpfValid from './isCpfValid';
 describe('isCpfValid', () => {
   describe('isCpfValid', () => {
     it('Should not throw error when validating 123.456.789-09', () => {
-      expect(() => isCpfValid('12345678909')).not.toThrow();
+      expect(isCpfValid('12345678909')).toBe(true);
     });
     it('Should throw error when validating CPF composed of the same numbers (111.111.111-11)', () => {
-      expect(() => isCpfValid('11111111111')).toThrow();
+      expect(isCpfValid('11111111111')).toBe(false);
     });
     it('Should throw error when the first validating digit is wrong (123.456.789-19)', () => {
-      expect(() => isCpfValid('12345678919')).toThrow();
+      expect(isCpfValid('12345678919')).toBe(false);
     });
     it('Should throw error when the second validating digit is wrong (123.456.789-08)', () => {
-      expect(() => isCpfValid('12345678908')).toThrow();
+      expect(isCpfValid('12345678908')).toBe(false);
     });
   });
 });
