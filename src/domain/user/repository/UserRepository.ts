23540386c296991a6db9c993315a3bcdfe8c9d8b
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-08 15:59:52 -0300

    refactor: changed acess to _database to database at UserRepository

--- src/domain/user/repository/UserRepository.ts
+++ src/domain/user/repository/UserRepository.ts
@@ -12,7 +12,7 @@ export default class UserRepository
   implements IRepositoryUser
 {
   public getNewIndex(): number {
-    const idArray = Array.from(this._database.keys());
+    const idArray = Array.from(this.database.keys());
     if (idArray.length === 0) {
       return 0;
     }
@@ -20,26 +20,26 @@ export default class UserRepository
   }
 
   public create(entity: IUser): IUser {
-    this._database.set(this.getNewIndex(), entity);
+    this.database.set(this.getNewIndex(), entity);
     return entity;
   }
 
   public read(id: number): undefined | IUser {
-    return this._database.get(id);
+    return this.database.get(id);
   }
 
   public readAll(): IVariableDatabase {
-    return this._database;
+    return this.database;
   }
 
   public update(id: number, newEntity: IUser): void {
-    this._database.set(id, newEntity);
+    this.database.set(id, newEntity);
   }
 
   public delete(id: number): void {
-    this._database.forEach((value: object, key: number) => {
+    this.database.forEach((value: object, key: number) => {
       if (key === id) {
-        this._database.delete(id);
+        this.database.delete(id);
       }
     });
   }
