1e56258d7bb35c9452046df34437d2d90fffe684
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-12 14:04:06 -0300

    refactor: added trycatch to ListUserController handle

--- src/presentation/controller/ListUserController.ts
+++ src/presentation/controller/ListUserController.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express';
+import StatusError from '@util/error';
+import { NextFunction, Request, Response } from 'express';
 import { IListUserService } from 'interfaces/domain/services/service';
 import { IEndPointsController } from 'interfaces/presentation/controller';
 import { inject, injectable } from 'tsyringe';
@@ -10,8 +11,16 @@ export default class ListUserController implements IEndPointsController {
     this.service = service;
   }
 
-  public handle(req: Request, res: Response): Response | undefined {
-    const info = this.service.readAll();
-    return res.status(200).send({ message: info });
+  public handle(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Response | undefined {
+    try {
+      const info = this.service.readAll();
+      return res.json({ message: info }).status(200);
+    } catch (error) {
+      next(new StatusError(500, `${error}`));
+    }
   }
 }
