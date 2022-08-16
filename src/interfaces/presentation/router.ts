88b7920c14621be4539861ebe729358aac103311
Neo-WRN <nicolas.destito@warren.com.br>
2022-08-16 16:24:40 -0300

    refactor: updated UserRouter, removing AbstractController and IUserRouter

--- src/interfaces/presentation/router.ts
+++ src/interfaces/presentation/router.ts
@@ -3,12 +3,5 @@ import { Router } from 'express';
 import { IEndPointsController } from './controller';
 
 export interface IEndPointsRouter {
-  router: Router;
   routes(): void;
-}
-
-export interface IRouterUser {
-  createUserController: IEndPointsController;
-  listUserController: IEndPointsController;
-  createMiddlewares: MiddlewareArray;
 }
